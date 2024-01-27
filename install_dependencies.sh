5e5dcb07b141284f0b06c746c8d4432c07e8d1a5
tomek-skrond <tomek2.skrond@gmail.com>
2024-01-27 02:30:06 +0100

    html/json/csv modes

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,12 +3,13 @@ install_dependencies(){
         if [[ "$ID_LIKE" =~ .*"rhel".* ]]; then
                 sudo yum install -y epel-release
 		sudo yum install -y perl-Image-ExifTool figlet
-		sudo yum install -y sqlite 
+		sudo yum install -y sqlite
+		sudo yum install -y poppler-utils
 #		install_drivers
         fi
 
         if [[ "$ID_LIKE" =~ .*"debian".* ]]; then
-                sudo apt install -y figlet exiftool sqlite3
+                sudo apt install -y figlet exiftool sqlite3 poppler-utils
         fi
 }
 install_dependencies
